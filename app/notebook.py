214763792525b08ea9e58f39eb31fdbfc16cfb5d
samuel <samuelgarcia201586@gmail.com>
2025-02-17 12:39:41 -0500

    primera comit

--- app/notebook.py
+++ app/notebook.py
@@ -1,1 +1,7 @@
 # TODO: Agrega el código de las clases del modelo aquí. Borra este comentario al terminar.
+
+class Note:
+    def __init__(self, Hihg: str, Medium: str, Low: str ):
+        self.Hihg:str = Hihg
+        self.Medium:str = Medium
+        self.Low:str = Low
