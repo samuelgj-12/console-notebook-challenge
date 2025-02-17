c3ccd3d84d383bcfbd25c717766567ebad774abc
samuel <samuelgarcia201586@gmail.com>
2025-02-17 12:59:14 -0500

    agregacion de atributos

--- app/notebook.py
+++ app/notebook.py
@@ -5,3 +5,20 @@ class Note:
         self.Hihg:str = Hihg
         self.Medium:str = Medium
         self.Low:str = Low
+
+    def __init__(self, code: str, title: str, text: str, importance: str, datetime:str):
+        self.code:str = code
+        self.title:str = title
+        self.text:str = text
+        self.importance:str = importance
+        self.creation_date:str = datetime.now()
+        self.tags = []
+
+    def add_tag(self, tag):
+        if tag not in self.tags:
+            self.tags.append(tag)
+
+    def __str__(self):
+        return f"Date: {self.creation_date}\n{self.title}: {self.text}"
+
+
